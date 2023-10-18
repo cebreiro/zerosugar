f8c257f0256489b9e6ae2d2837742fef61d79d03
Beco <ghewqa1234@gmail.com>
2023-10-18 22:46:32 +0900

    Fix white space character
    - tab to space

--- cmake/msvc.cmake
+++ cmake/msvc.cmake
@@ -18,6 +18,6 @@ elseif (POLICY CMP0141)
 
     # Enable Hot Reload for MSVC compilers if supported.
     cmake_policy(SET CMP0141 NEW)
-	set(CMAKE_MSVC_DEBUG_INFORMATION_FORMAT "$<IF:$<AND:$<C_COMPILER_ID:MSVC>,$<CXX_COMPILER_ID:MSVC>>,$<$<CONFIG:Debug,RelWithDebInfo>:EditAndContinue>,$<$<CONFIG:Debug,RelWithDebInfo>:ProgramDatabase>>")
+    set(CMAKE_MSVC_DEBUG_INFORMATION_FORMAT "$<IF:$<AND:$<C_COMPILER_ID:MSVC>,$<CXX_COMPILER_ID:MSVC>>,$<$<CONFIG:Debug,RelWithDebInfo>:EditAndContinue>,$<$<CONFIG:Debug,RelWithDebInfo>:ProgramDatabase>>")
 
 endif ()
