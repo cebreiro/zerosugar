90e1bc2268b7a42b9162dbfc6446b2edbcf2586d
Beco <ghewqa1234@gmail.com>
2023-10-21 00:34:05 +0900

    Configure build-configure-release-pdb-generation
    - for debugging, add compile and linker option

--- cmake/msvc.cmake
+++ cmake/msvc.cmake
@@ -11,11 +11,14 @@ add_compile_definitions(
 )
 
 if (CMAKE_CXX_FLAGS MATCHES "-fsanitize=address")
-
     add_link_options(/INCREMENTAL:NO)
 
-elseif (POLICY CMP0141)
+elseif (CMAKE_CONFIGURATION_TYPES STREQUAL Release)
+    #set(CMAKE_MSVC_DEBUG_INFORMATION_FORMAT ProgramDatabase)
+    add_compile_options(/Zi)
+    add_link_options(/DEBUG /OPT:REF /OPT:ICF)
 
+elseif (POLICY CMP0141)
     # Enable Hot Reload for MSVC compilers if supported.
     cmake_policy(SET CMP0141 NEW)
     set(CMAKE_MSVC_DEBUG_INFORMATION_FORMAT "$<IF:$<AND:$<C_COMPILER_ID:MSVC>,$<CXX_COMPILER_ID:MSVC>>,$<$<CONFIG:Debug,RelWithDebInfo>:EditAndContinue>,$<$<CONFIG:Debug,RelWithDebInfo>:ProgramDatabase>>")
