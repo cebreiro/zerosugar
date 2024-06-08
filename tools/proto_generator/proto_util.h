06f49e15ce63bd5a76ac9edcacb5c1ddd97df811
Beco <ghewqa1234@gmail.com>
2024-06-08 22:01:14 +0900

    Remove unused files

--- tools/proto_generator/proto_util.h
+++
@@ -1,7 +0,0 @@
-#pragma once
-#include <string>
-
-namespace zerosugar::proto_util
-{
-    auto ConvertPackageToNamespace(const std::string& package) -> std::string;
-}
--- tools/proto_generator/string_util.h
+++
@@ -1,7 +0,0 @@
-#pragma once
-#include <string>
-
-namespace zerosugar::string_util
-{
-    auto ToSnakeCase(const std::string& str) -> std::string;
-}
